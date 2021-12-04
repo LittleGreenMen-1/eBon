1ba1a77a4e4b243dbc1bc0f28102e4845a035b38
VladTomici14 <vladtomici@gmail.com>
2021-12-05 00:43:47 +0200

    Added new files.

---
+++ Code/RPi-Code/company.py
@@ -0,0 +1,182 @@
+"""
+sanatate
+divertisment
+transport
+restaurant
+supermarket
+electronice IT
+servicii
+altele
+"""
+
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+
+CAEN_to_domain = {
+    "01": "Agricultura, vanatoare si servicii anexe",
+    "02": "Silvicultura si exploatare forestiera",
+    "03": "Pescuitul si acvacultura",
+    "05": "Extractia carbunelui superior si inferior",
+    "06": "Extractia petrolului brut si a gazelor naturale",
+    "07": "Extractia minereurilor metalifere",
+    "08": "Alte activitati extractive",
+    "09": "Activitati de servicii anexe extractiei",
+    "10": "Industria alimentara",
+    "11": "Fabricarea bauturilor",
+    "12": "Fabricarea produselor din tutun",
+    "13": "Fabricarea produselor textile",
+    "14": "Fabricarea articolelor de imbracaminte",
+    "15": "Tabacirea si finisarea pieilor; fabricarea articolelor de voiaj si marochinarie, harnasamentelor si incaltamintei; prepararea si vopsirea blanurilor",
+    "16": "Prelucrarea lemnului, fabricarea produselor din lemn si pluta, cu exceptia mobilei; fabricarea articolelor din paie si din alte materiale vegetale impletite",
+    "17": "Fabricarea hartiei si a produselor din hartie",
+    "18": "Tiparire si reproducerea pe suporti a inregistrarilor",
+    "19": "Fabricarea produselor de cocserie si a produselor obtinute din prelucrarea titeiului",
+    "20": "Fabricarea substantelor si a produselor chimice",
+    "21": "Fabricarea produselor farmaceutice de baza si a preparatelor farmaceutice",
+    "22": "Fabricarea produselor din cauciuc si mase plastice",
+    "23": "Fabricarea altor produse din minerale nemetalice",
+    "24": "Industria metalurgica",
+    "25": "Industria constructiilor metalice si a produselor din metal, exclusiv masini, utilaje si instalatii",
+    "26": "Fabricarea calculatoarelor si a produselor electronice si optice",
+    "27": "Fabricarea echipamentelor electrice",
+    "28": "Fabricarea de masini, utilaje si echipamente n.c.a.",
+    "29": "Fabricarea autovehiculelor de transport rutier, a remorcilor si semiremorcilor",
+    "30": "Fabricarea altor mijloace de transport",
+    "31": "Fabricarea de mobila",
+    "32": "Alte activitati industriale n.c.a.",
+    "33": "Repararea, intretinerea si instalarea masinilor si echipamentelor",
+    "35": "Productia si furnizarea de energie electrica si termica, gaze, apa calda si aer conditionat",
+    "36": "Captarea, tratarea si distributia apei",
+    "37": "Colectarea si epurarea apelor uzate",
+    "38": "Colectarea, tratarea si eliminarea deseurilor; activitati de recuperare a materialelor reciclabile",
+    "39": "Activitati si servicii de decontaminare",
+    "41": "Constructii de cladiri",
+    "42": "Lucrari de geniu civil",
+    "43": "Lucrari speciale de constructii",
+    "45": "Comert cu ridicata si cu amanuntul, intretinerea si repararea autovehiculelor si a motocicletelor",
+    "46": "Comert cu ridicata cu exceptia comertului cu autovehicule si motociclete",
+    "47": "Comert cu amanuntul, cu exceptia autovehiculelor si motocicletelor",
+    "49": "Transporturi terestre si transporturi prin conducte",
+    "50": "Transporturi pe apa",
+    "51": "Transporturi aeriene",
+    "52": "Depozitare si activitati auxiliare pentru transporturi",
+    "53": "Activitati de posta si de curier",
+    "55": "Hoteluri si alte facilitati de cazare",
+    "56": "Restaurante si alte activitati de servicii de alimentatie",
+    "58": "Activitati de editare",
+    "59": "Activitati de productie cinematografica, video si de programe de televiziune; inregistrari audio si activitati de editare muzicala",
+    "60": "Activitati de difuzare si transmitere de programe",
+    "61": "Telecomunicatii",
+    "62": "Activitati de servicii in tehnologia informatiei",
+    "63": "Activitati de servicii informatice",
+    "64": "Intermedieri financiare, cu exceptia activitatilor de asigurari si ale fondurilor de pensii",
+    "65": "Activitati de asigurari, reasigurari si ale fondurilor de pensii (cu exceptia celor din sistemul public de asigurari sociale)",
+    "66": "Activitati auxiliare pentru intermedieri financiare, activitati de asigurare si fonduri de pensii",
+    "68": "Tranzactii imobiliare",
+    "69": "Activitati juridice si de contabilitate",
+    "70": "Activitati ale directiilor(centralelor), birourilor administrative centralizate; activitati de management si de consultanta in management",
+    "71": "Activitati de arhitectura si inginerie; activitati de testari si analiza tehnica",
+    "72": "Cercetare",
+    "73": "Publicitate si activitati de studiere a pietei",
+    "74": "Alte activitati profesionale, stiintifice si tehnice",
+    "75": "Activitati veterinare",
+    "77": "Activitati de inchiriere si leasing",
+    "78": "Activitati de servicii privind forta de munca",
+    "79": "Activitati ale agentiilor turistice si a tur",
+    "80": "Activitati de investigatii si protectie",
+    "81": "Activitati de peisagistica si servicii pentru cladiri",
+    "82": "Activitati de secretariat, servicii suport si alte activitati de servicii prestate in principal intreprinderilor",
+    "84": "Administratie publica si aparare; asigurari sociale din sistemul public",
+    "85": "Invatamant",
+    "86": "Activitati referitoare la sanatatea umana",
+    "87": "Servicii combinate de ingrijire medicala si asistenta sociala, cu cazare",
+    "88": "Activitati de asistenta sociala, fara cazare",
+    "90": "Activitati de creatie si interpretare artistica",
+    "91": "Activitati ale bibliotecilor, arhivelor, muzeelor si alte activitati culturale",
+    "92": "Activitati de jocuri de noroc si pariuri",
+    "93": "Activitati spotive, recreative si distractive",
+    "94": "Activitati asociative diverse",
+    "95": "Reparatii de calculatoare, de articole personale si de uz gospodaresc",
+    "96": "Alte activitati de servicii",
+    "97": "Activitati ale gospodariilor private in calitate de angajator de personal casnic",
+    "98": "Activitati ale gospodariilor private de producere de bunuri si servicii destinate consumului propriu",
+    "99": "Activitati ale organizatiilor si organismelor extrateritoriale",
+}
+
+
+def getDomenii():
+    url = "https://www.infoquick.ro/verificare-firme-domeniu-activitate"
+
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    raw_html = urlopen(req).read()
+    html = BeautifulSoup(raw_html, features="html.parser")
+
+    domenii = html.find_all("a", {"class": "cl-red"})
+
+    with open("Domains.txt", 'w') as file:
+        file.write("{\n")
+        for domeniu in domenii:
+            data = domeniu.text.split("-")
+            CAEN = data[0].strip()[0:2]
+            description = data[1].strip()
+
+            file.write(f"\t\"{CAEN}\": \"{description}\",\n")
+        file.write("}")
+
+
+def extractFromCUI(cui):
+    url = f"https://www.confidas.ro/cauta-firma?q={cui}"
+
+    request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    raw_html = urlopen(request).read()
+    html = BeautifulSoup(raw_html, features="html.parser")
+
+    td = html.find_all("td", {"data-th": "Firme"})[0]
+    link = td.findChildren("a", recursive=False)[0]
+    url = link["href"]
+
+    request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    raw_html = urlopen(request).read()
+    html = BeautifulSoup(raw_html, features="html.parser")
+
+    CAEN_div = html.find_all("div", {"class": "column is-6"})[1]
+    content_div = CAEN_div.findChildren("div", recursive=True)[2]
+    CAEN = content_div.findChildren("span", recursive=True)[1].text
+
+    import re
+
+    CAEN = re.findall("[0-9]{4}", CAEN)[0]
+    domeniu = CAEN_to_domain[CAEN[0:2]]
+
+    print(domeniu)
+
+
+def extractDescription(cui):
+    url = f"https://www.risco.ro/verifica-firma/{name}-cui-{cui}"
+    request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html = urlopen(request).read()
+    soup = BeautifulSoup(html, features="html.parser")
+
+    for script in soup(["script", "style"]):
+        script.extract()
+
+    text = soup.get_text()
+
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split(" "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    word = ""
+    sentence = ""
+    index1 = text.find("Descriere:") + len("Descriere:") + 1
+    index2 = text.find("Adresa:")
+    for i in range(index1, index2):
+        if text[i] == '\n':
+            sentence += (word + " ")
+            word = ""
+        if text[i] != '\n':
+            word += text[i]
+
+    print(sentence)
+
+extractFromCUI("20119287")
