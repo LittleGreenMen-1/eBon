c98177997dca67d3196d3cdd2497cd6e7194032d
sirbuandrei <60944128+sirbuandrei@users.noreply.github.com>
2021-12-05 05:58:50 +0200

    Add files via upload
    new home page

--- FlutterApp/e_bon/lib/pages/home_page.dart
+++ FlutterApp/e_bon/lib/pages/home_page.dart
@@ -15,64 +15,36 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  final PageController _controller = PageController(
-    initialPage: 1,
-  );
-
-  //TODO: schimba style-ul la butoane dupa ce sunt apasate cu enum
-  //TODO: ADADADA
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('eBon'),
-        centerTitle: true,
-        /*actions: [
-          IconButton(
-              onPressed: () async{
-                await _auth.signOut();
-              },
-              icon: const Icon(Icons.logout))
-        ],*/
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                HomeButton(buttonTitle: 'Bonuri', onTap: (){
-                  setState(() {
-                    _controller.animateToPage(0, duration: kDuration, curve: kCurve);
-                  });
-                },),
-                HomeButton(buttonTitle: 'Scan', onTap: (){
-                  setState(() {
-                    _controller.animateToPage(1, duration: kDuration, curve: kCurve);
-                  });
-                },),
-                HomeButton(buttonTitle: 'Statistici', onTap: (){
-                  setState(() {
-                    _controller.animateToPage(2, duration: kDuration, curve: kCurve);
-                  });
-                },),
-              ],
-            ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('eBon'),
+          centerTitle: true,
+          /*actions: [
+            IconButton(
+                onPressed: () async{
+                  await _auth.signOut();
+                },
+                icon: const Icon(Icons.logout))
+          ],*/
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Bonuri'),
+              Tab(text: 'Scan'),
+              Tab(text: 'Statistici')
+            ],
           ),
-          Expanded(
-            flex: 12,
-            child: PageView(
-              controller: _controller,
-              children: const [
-                ReceiptsPage(),
-                ScanPage(),
-                StatsPage()
-              ],
-            ),
-          ),
-        ],
+        ),
+        body: const TabBarView(
+          children: [
+            ReceiptsPage(),
+            ScanPage(),
+            StatsPage()
+          ],
+        ),
       ),
     );
   }
